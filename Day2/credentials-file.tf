091183e4ac79e87c880793a0963f3126bea99b0c
adesh202 <adeshkumar2501@gmail.com>
2024-09-19 16:42:21 +0530

    Day2 Progress Multiple Ways to Authenticate Providers

---
+++ Day2/credentials-file.tf
@@ -0,0 +1,10 @@
+//option1
+provider "aws" {
+  shared_config_files      = ["C:/Users/Adesh Chauhan/.aws/config"]
+  shared_credentials_files = ["C:/Users/Adesh Chauhan/.aws/credentials"]
+  profile                  = "default"
+}
+
+//option1
+//provider "aws" {
+//}
---
+++ Day2/environment-variables.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    
+}
+
+//% export AWS_ACCESS_KEY_ID="anaccesskey"
+//% export AWS_SECRET_ACCESS_KEY="asecretkey"
+//% export AWS_REGION="us-west-2"
+//% terraform plan
